2c5654642eda59dafb72a839d0d67c2bf3539eff
Pratham7867 <prathamgedam06@gmail.com>
2025-09-24 17:43:35 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,240 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { Sky } from 'three/addons/objects/Sky.js';
+import vertexShader from './Shader/vertex.glsl'
+import fragmentShader from './Shader/Fragment.glsl'
+import gsap from 'gsap'
+
+
+/**
+ * Base
+ */
+// Debug
+// const gui = new GUI({ width: 340 })
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+// scene.resolution = new THREE.Vector2(sizes.width, sizes.height)
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    // sizes.resolution=(sizes.width, sizes.height)
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1.5, 0, 6)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+//Texture 
+const Texture =
+    [
+        textureLoader.load('./particles/1.png'),
+        textureLoader.load('./particles/2.png'),
+        textureLoader.load('./particles/3.png'),
+        textureLoader.load('./particles/4.png'),
+        textureLoader.load('./particles/5.png'),
+        textureLoader.load('./particles/6.png'),
+        textureLoader.load('./particles/7.png'),
+        textureLoader.load('./particles/8.png'),
+    ]
+// Fire Work
+const createFirework = (count, position, size, Texture, radius, Color) => {
+    const PositionArray = new Float32Array(count * 3);
+    const sizesArray = new Float32Array(count);
+    const timeMultiplier = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+
+        const Spericle = new THREE.Spherical(
+            radius * (0.75 + Math.random() * 0.25),
+            Math.random() * Math.PI,
+            Math.random() * Math.PI * 2,
+        )
+        const Position = new THREE.Vector3()
+        Position.setFromSpherical(Spericle)
+
+        // Scatter in a square area on the XY plane
+        PositionArray[i3] = Position.x; // X: -1 to 1
+        PositionArray[i3 + 1] = Position.y; // Y: -1 to 1
+        PositionArray[i3 + 2] = Position.z;                         // Z: flat on Z-plane
+
+
+        sizesArray[i] = Math.random()
+        timeMultiplier[i] = 1 + Math.random()
+    }
+
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(PositionArray, 3));
+    geometry.setAttribute('aSize', new THREE.Float32BufferAttribute(sizesArray, 1));
+    geometry.setAttribute('aTime', new THREE.Float32BufferAttribute(timeMultiplier, 1));
+    Texture.flipY = false
+    const material = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms: ({
+            uSize: new THREE.Uniform(size),
+            // uRsolution:new THREE.Uniform(sizes.resolution)
+            uTexture: new THREE.Uniform(Texture),
+            uColor: new THREE.Uniform(Color),
+            uProgress: new THREE.Uniform(0)
+        }),
+        transparent: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+
+    });
+
+    const firework = new THREE.Points(geometry, material);
+    firework.position.copy(position);
+    scene.add(firework);
+
+
+    //despose
+    const Distroy = () => {
+
+        scene.remove(firework)
+        geometry.dispose()
+        material.dispose()
+    }
+
+
+    // Animation
+    gsap.to(
+        material.uniforms.uProgress,
+        { value: 1, duration: 3, ease: 'linear', onComplete: Distroy }
+    )
+
+};
+
+
+const createRandomfirework = () => {
+    const count = Math.round(400 + Math.random() * 1000)
+    const position = new THREE.Vector3(
+        (Math.random() - 0.5) * 2,
+        Math.random(),
+        (Math.random() - 0.5) * 2
+    )
+    const size = 100 + Math.random() * 0.1
+    const texture = Texture[Math.floor(Math.random() * Texture.length)];
+    const radius = 0.5 + Math.random()
+    const color = new THREE.Color()
+    color.setHSL(Math.random(), 1, 0.7)
+
+    createFirework(count, position, size, texture, radius, color)
+}
+
+createRandomfirework()
+// click
+window.addEventListener('click', createRandomfirework)
+
+//sky 
+// Add Sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+
+/// GUI
+
+const effectController = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.95,
+    elevation: -2.2,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+};
+
+function guiChanged() {
+
+    const uniforms = sky.material.uniforms;
+    uniforms['turbidity'].value = effectController.turbidity;
+    uniforms['rayleigh'].value = effectController.rayleigh;
+    uniforms['mieCoefficient'].value = effectController.mieCoefficient;
+    uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
+
+    const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+    const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    uniforms['sunPosition'].value.copy(sun);
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render(scene, camera);
+
+}
+
+
+// gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged);
+// gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged);
+// gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(guiChanged);
+// gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(guiChanged);
+// gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged);
+// gui.add(effectController, 'azimuth', - 180, 180, 0.1).onChange(guiChanged);
+// gui.add(effectController, 'exposure', 0, 1, 0.0001).onChange(guiChanged);
+
+guiChanged();
+
+/**
+ * Animate
+ */
+const tick = () => {
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
